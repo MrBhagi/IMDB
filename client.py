b66602a860494679ff6053ed70f6e79ed5b6fe3c
Dminor7 <contact.dshukla@gmail.com>
2022-10-12 13:48:06 +0530

    Adding file extension

--- client.py
+++ client.py
@@ -5,6 +5,6 @@ from imdb import Extract, Parse, Load
 
 if __name__ == '__main__':
     input_file, output_file = sys.argv[1:]
-    pages = Extract(input_file=pathlib.Path(f"input/{input_file}")).execute()
+    pages = Extract(input_file=pathlib.Path(f"input/{input_file}.csv")).execute()
     df =  Parse(html_pages=pages).execute()
-    Load(output_filename=pathlib.Path(f"output/{output_file}"), df=df).execute()
+    Load(output_filename=pathlib.Path(f"output/{output_file}.csv"), df=df).execute()
